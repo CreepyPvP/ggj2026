f25b90c884b3bdf4b3f49bae4bca507bfe64512c
Luis Dietl <interruptedThread@gmail.com>
2026-01-31 14:23:42 +0100

    Cone drawing

--- code/game.cpp
+++ code/game.cpp
@@ -254,26 +254,31 @@ void UpdateCamera(const Entity *entity, f32 delta)
 
 static void GameDrawCone(Vector2 pos, Vector2 dir, f32 length, f32 angle)
 {
-    u32 sample_points = 100;
-    Vector2 point_buffer[128];
+    u32 sample_points = 128;
 
     f32 forward_angle = 0;
-    f32 start_angle = forward_angle - angle / 2;
-    f32 end_angle = forward_angle + angle / 2;
+    f32 start_angle = forward_angle + angle / 2;
+    f32 end_angle = forward_angle - angle / 2;
 
-    for (u32 i = 0; i < sample_points; ++i)
+    Vector2 prev_sample;
+
     {
-        f32 current_angle = start_angle + (end_angle - start_angle) * ((f32) i / ((f32) sample_points - 1));
-        Vector2 current_direction = {cos(current_angle / 180.0f * PI), sin(current_angle / 180.0f * PI)};
-
-        f32 len = GameRaycast(pos, current_direction);
-
-        point_buffer[i] = (pos + current_direction * len) * 32;
+        Vector2 current_dir = {cos(start_angle / 180.0f * PI), sin(start_angle / 180.0f * PI)};
+        f32 len = Min(GameRaycast(pos, current_dir), length);
+        prev_sample = (pos + current_dir * len) * 32;
     }
 
-    point_buffer[sample_points] = pos * 32;
+    for (u32 i = 1; i < sample_points; ++i)
+    {
+        f32 current_angle = start_angle + (end_angle - start_angle) * ((f32) i / ((f32) sample_points - 1));
+        Vector2 current_dir = {cos(current_angle / 180.0f * PI), sin(current_angle / 180.0f * PI)};
 
-    DrawTriangleFan(point_buffer, sample_points, BLUE);   
+        f32 len = Min(GameRaycast(pos, current_dir), length);
+        Vector2 sample = (pos + current_dir * len) * 32;
+
+        DrawTriangle(pos * 32, prev_sample, sample, Fade(BLUE, 0.5));
+        prev_sample = sample;
+    }
 }
 
 static void GameFrame(f32 delta)
@@ -327,7 +332,7 @@ static void GameFrame(f32 delta)
     }
 
     if (PLAYER != NULL)
-        GameDrawCone(PLAYER->position, {1, 0}, 3, 45);
+        GameDrawCone(PLAYER->position, {1, 0}, 6, 45);
 
     EndMode2D();
 
