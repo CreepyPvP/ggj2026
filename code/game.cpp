625fdb5a25f1c53cc60fc30c3d4111e86ce7ccc3
Luis Dietl <interruptedThread@gmail.com>
2026-01-31 09:45:32 +0100

    Loading autolayers

--- code/game.cpp
+++ code/game.cpp
@@ -44,49 +44,6 @@ u8 GetTile(i32 x, i32 y)
 
 void LoadWorld()
 {
-#if 0
-    ldtk::Project ldtk_project;
-    try {
-        ldtk_project.loadFromFile("assets/test/testWorld.ldtk");
-    }
-    catch (std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
-    }
-
-    const auto& world = ldtk_project.getWorld();
-
-    auto worlds = ldtk_project.allWorlds();
-
-    // Drawing level 1
-    auto levels = worlds[0].allLevels();
-    auto layers = levels[0].allLayers();
-
-    for (auto& layer : layers) {
-        Texture2D texture = LoadTexture(("assets/" + layer.getTileset().path).c_str());
-        RenderTexture2D renderer = LoadRenderTexture(levels[0].size.x, levels[0].size.y);
-        auto& tiles = layer.allTiles();
-
-        BeginTextureMode(renderer);
-        ClearBackground(BLACK);
-
-        for (const auto& tile : tiles) {
-            const auto& position = tile.getPosition();
-            const auto& texture_rect = tile.getTextureRect();
-
-            Vector2 dest = Vector2(static_cast<float>(position.x), static_cast<float>(position.y));
-            Rectangle src = {
-                static_cast<float>(texture_rect.x),
-                static_cast<float>(texture_rect.y),
-                static_cast<float>(texture_rect.width) * (tile.flipX ? -1.0f : 1.0f),
-                static_cast<float>(texture_rect.height) * (tile.flipY ? -1.0f : 1.0f)
-            };
-
-            DrawTextureRec(texture, src, dest, WHITE);
-        }
-        EndTextureMode();
-    }
-#endif
-
     ldtk::Project ldtk_project;
     ldtk_project.loadFromFile("assets/world/game_world.ldtk");
     const auto& world = ldtk_project.getWorld("world");
@@ -105,11 +62,25 @@ void LoadWorld()
         }
     }
 
-    // for (const auto& tile : collision_layer.allTiles()) {
-    //     ldtk::Point<int> position = tile.getPosition();
-    //     if (position.x >= 0 && position.y >= 0)
-    //         SetTile(position.x, position.y, tile.tileId);
-    // }
+    for (const ldtk::Layer& layer : level.allLayers())
+    {
+        if (layer.getType() == ldtk::LayerType::AutoLayer)
+        {
+            for (const auto& tile : layer.allTiles()) {
+                const auto& position = tile.getPosition();
+                const auto& texture_rect = tile.getTextureRect();
+
+                Vector2 dest = Vector2(position.x, position.y);
+                Rectangle src = {
+                    (f32) texture_rect.x,
+                    (f32) texture_rect.y,
+                    (f32) texture_rect.width * (tile.flipX ? -1.0f : 1.0f),
+                    (f32) texture_rect.height * (tile.flipY ? -1.0f : 1.0f)
+                };
+            }
+        }
+    }
+
 
     // // iterate on Enemy entities
     // for (const ldtk::Entity& enemy : level1.getLayer("Entities").getEntitiesByName("Enemy")) {
